aa665b630d6802e8a331bc9c73068ede23ee65d7
iDaDi <564375286@qq.com>
2021-08-10 19:07:31 +0800

    测试修改提交

--- Source/CMyGame/CMyGameGameModeBase.cpp
+++ Source/CMyGame/CMyGameGameModeBase.cpp
@@ -3,3 +3,30 @@
 
 #include "CMyGameGameModeBase.h"
 
+
+void ACMyGameGameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+	ChanageMenuWidget(StartingWidgetClass);
+}
+
+
+void ACMyGameGameModeBase::ChanageMenuWidget(TSubclassOf<UUserWidget> NewWidgetClass)
+{
+	if (CurrWidget != nullptr)
+	{
+		CurrWidget->RemoveFromViewport();
+		CurrWidget = nullptr;
+	}
+
+	if (NewWidgetClass != nullptr)
+	{
+		CurrWidget = CreateWidget(GetWorld(), NewWidgetClass);
+		//CurrWidget = CreateWidget<UUserWidget>(GetWorld(), NewWidgetClass);
+		if (CurrWidget != nullptr)
+		{
+			CurrWidget->AddToViewport();
+		}
+	}
+}
+
--- Source/CMyGame/CMyGameGameModeBase.h
+++ Source/CMyGame/CMyGameGameModeBase.h
@@ -4,14 +4,28 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "Blueprint/UserWidget.h"
 #include "CMyGameGameModeBase.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class CMYGAME_API ACMyGameGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	UFUNCTION(BlueprintCallable,Category = "UMG Game")
+		void ChanageMenuWidget(TSubclassOf<UUserWidget> NewWidgetClass);
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category = "UMG Game")
+	TSubclassOf<UUserWidget> StartingWidgetClass;
+
+	UPROPERTY()
+	UUserWidget* CurrWidget;
+
 };
